9e58b13389c4bc1d74a5c7f5ef07465cd35f1448
AlexOlukoya <alex.olukoya@gmail.com>
2020-04-04 17:12:57 -0700

    NumberFormat

--- src/edu/whccd/aolukoya/Main.java
+++ src/edu/whccd/aolukoya/Main.java
@@ -1,41 +1,45 @@
 package edu.whccd.aolukoya;
 
 import java.util.Scanner;
+import java.text.NumberFormat;
 
 /*
-The purpose of this application is to replicate the code in Figure 2-17
+The purpose of this application is to replicate the code in Figure 3-13
 author:     Alex Olukoya
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Invoice Total Calculator");
-        System.out.println();
+        final double SALES_TAX_PCT = .05;
 
         Scanner sc = new Scanner(System.in);
         String choice = "y";
         while (choice.equalsIgnoreCase("y")) {
-
-            System.out.print("Enter subtotal:  ");
+            System.out.print("Enter subtotal:   ");
             double subtotal = sc.nextDouble();
 
-            double discountPercent;
-            if (subtotal >= 200) {
-                discountPercent = .2;
-            } else if (subtotal >= 100) {
+            double discountPercent = 0.0;
+            if (subtotal >= 100) {
                 discountPercent = .1;
             } else {
                 discountPercent = 0.0;
             }
             double discountAmount = subtotal * discountPercent;
-            double total = subtotal - discountAmount;
+            double totalBeforeTax = subtotal - discountAmount;
+            double salesTax = totalBeforeTax * SALES_TAX_PCT;
+            double total = totalBeforeTax + salesTax;
 
-            String message = "Discount percent: " + discountPercent + "\n"
-                           + "Discount amount:  " + discountAmount + "\n"
-                           + "Invoice total:    " + total + "\n";
+            NumberFormat currency = NumberFormat.getCurrencyInstance();
+            NumberFormat percent = NumberFormat.getPercentInstance();
+            String message =
+                    "Discount percent:" + percent.format(discountPercent) + "\n"
+                            + "Discount amount:    " + currency.format(discountAmount) + "\n"
+                            + "Total before tax:   " + currency.format(totalBeforeTax) + "\n"
+                            + "Sales tax:          " + currency.format(salesTax) + "\n"
+                            + "Invoice total:      " + currency.format(total) + "\n";
             System.out.println(message);
 
-            System.out.print("Continue? (y/n) : ");
+            System.out.print("Continue? (y/n): ");
             choice = sc.next();
             System.out.println();
         }
